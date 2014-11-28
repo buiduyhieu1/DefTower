35576f58f022f79093965cdfe6ee6e4440f500bd
BuiDuyHIeu <duyhieu3193@gmail.com>
2014-11-28 22:56:59 +0700

    test auto commit 2

--- rungit.bat
+++ rungit.bat
@@ -1,6 +1,7 @@
+@echo off
+set /p msg= message commit:
 for /f "delims=" %%D in ('dir /a:d /b') do git add %%D
 for %%f in (*.*) do git add %%f
-git commit -am "test auto run"
-git remote add hsv git@github.com:buiduyhieu1/DefTower.git
-git push -u hsv master
+git commit -am "%msg%"
+echo done!
 pause
